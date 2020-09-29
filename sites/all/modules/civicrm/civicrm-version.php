1ceabac995b508d93b37f5cddf733070599395b5
Almost Engineer <bitsecondal@gmail.com>
2020-09-28 20:08:26 -0500

    #24 Removed remaining Drupal files

--- sites/all/modules/civicrm/civicrm-version.php
+++
@@ -1,8 +0,0 @@
-<?php
-/** @deprecated */
-function civicrmVersion( ) {
-  return array( 'version'  => '5.7.2',
-                'cms'      => 'Drupal',
-                'revision' => '' );
-}
-
--- sites/all/modules/custom/nsbemp.org/README.txt
+++
@@ -1,1 +0,0 @@
-Directory is to store backups of rules, features, and views that are used on the website.
