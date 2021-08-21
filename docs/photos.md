055b59ceee4c73a4e5469d12bf3dd40b3998f58d
Almost Engineer <bitsecondal@gmail.com>
2021-08-20 22:29:14 -0500

    Closed #38 Moved to Bootstrap icons

--- docs/photos.md
+++ docs/photos.md
@@ -7,8 +7,8 @@ imagealt: Members of NSBE MP and ASU NSBE
 To see our photos from past events, please visit our social media pages.
 
 <a class="btn text-white" href="https://www.facebook.com/nsbemp" target="_blank">
-<i class="fab fa-facebook"></i> Facebook</a>
+<i class="bi bi-facebook"></i> Facebook</a>
 <a class="text-white btn" href="https://www.instagram.com/nsbemp" target="_blank">
-<i class="fab fa-instagram"></i> Instagram</a>
+<i class="bi bi-instagram"></i> Instagram</a>
 <a class="text-white btn" href="https://twitter.com/nsbemp" target="_blank">
-<i class="fab fa-twitter"></i> Twitter</a>
+<i class="bi bi-twitter"></i> Twitter</a>
--- nsbemptheme/contact.php
+++ nsbemptheme/contact.php
@@ -55,11 +55,11 @@ if (isset($_POST['emailaddress']) && isset($HELPDESK_EMAIL)) {
     </p>
     <p>
         <a class="btn text-white" href="https://www.facebook.com/nsbemp" target="_blank">
-            <i class="fab fa-facebook"></i> Facebook</a><span> </span>
+            <i class="bi bi-facebook"></i> Facebook</a><span> </span>
         <a class="text-white btn" href="https://www.instagram.com/nsbemp" target="_blank">
-            <i class="fab fa-instagram"></i> Instagram</a><span> </span>
+            <i class="bi bi-instagram"></i> Instagram</a><span> </span>
         <a class="text-white btn" href="https://twitter.com/nsbemp" target="_blank">
-            <i class="fab fa-twitter"></i> Twitter</a>
+            <i class="bi bi-twitter"></i> Twitter</a>
     </p>
     <form method="POST" action="/contact.php">
         <p>
